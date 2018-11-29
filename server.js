46bde3a90c659c52a93e1b26738d649cbb64e976
in hyub jeon <inhyub@inhyubs-MacBook-Air.local>
2018-11-28 19:51:50 -0800

    added artillery and new relic

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
+require('newrelic');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const app = express();
-const port = process.env.PORT || 1000;
+const proxy = require('http-proxy-middleware');
+const compression = require('compression');
 
-app.use(morgan('dev'));
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(compression());
+
+//app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/:id/reservation', 
+  proxy({
+    target: 'http://localhost:3002',
+    changeOrigin: true
+  })
+);
+
+app.use('/api/:id/hour',
+  proxy({
+    target: 'http://localhost:3002',
+    changeOrigin: true
+  })
+);
 
 app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
